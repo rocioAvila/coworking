de2693f0e417bc705395a446b7516c8a0caaad00
RocioAvila <rocioa.avila98@gmail.com>
2025-01-23 21:40:40 -0300

    first commit

---
+++ src/main/java/com/appcoworking/Demo/de/appCoworking/DemoDeAppCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.appcoworking.Demo.de.appCoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeAppCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeAppCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appcoworking/Demo/de/appCoworking/DemoDeAppCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcoworking.Demo.de.appCoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeAppCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
